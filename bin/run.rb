c0f959778e9558e095b1b79c986b9d8706a6aaee
Maximo Bautista <max.bautista97@gmail.com>
2019-01-29 20:05:08 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,15 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby.
 
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+require "pry"
+
 
 welcome
 character = get_character_from_user
-show_character_movies(character)
+movie = show_character_movies(character)
+details = get_movies_detail_from_api(character)
+
+if movie.length == 0 && details != 1
+  puts "Character or movie not found."
+end
